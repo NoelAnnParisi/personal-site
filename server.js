35824a2cf1fff0fa5a4c74cfe28b62c4c99b8c49
Noel Parisi <ycleptnoel@gmail.com>
2017-06-16 12:52:11 -0700

    added express server/webpack

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
+
+
+app.listen(PORT, (err)=> {
+	if (!err){
+		console.log(`YAHS QWEEN!`);
+	}
+})
